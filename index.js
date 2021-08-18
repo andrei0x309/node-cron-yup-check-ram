5a68b5d267bb45e0476bff89b8acec7fa909da44
Andrei O. (andrei0x309) <andrei@flashsoft.eu>
2021-08-18 18:28:50 +0300

    added more types of checks and stats

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ const TOKEN = process.env.TOKEN;
 const supabase = createClient("https://lcspcmmpolegvalxkfsu.supabase.co", TOKEN)
 
 // Enough for at least ~ one transaction
-const mimimumRamRequired = 800;
+const mimimumRamRequired = 750; // YUP Protocol
+// Enough for at least ~ two transaction
+const mimimumRamRequiredManager = 400; // YUP ACC manager
 
 // Main EOS
 const rcpOptions = {
@@ -31,7 +33,7 @@ const eosApi = EosApi(rcpOptions);
 
 const checkRam = async (supabase,interval) => {
   
-  const { count, error } = await supabase.from('uptime-check-record').select('*', { count: 'exact', head: true })
+  const { count, error } = await supabase.from('uptime-check-record').select('*', { count: 'exact', head: true });
   if(error){
     // Database error stop execution probably should ping a service worker to get notified
     clearInterval(interval);
@@ -39,7 +41,7 @@ const checkRam = async (supabase,interval) => {
   }
 
   if(count > 2000){
-    const { data } = await supabase.from('uptime-check-record').select('id').limit(1);
+    const { data } = await supabase.from('uptime-check-record').select('id').select('*').order('check-date', { ascending: true }).limit(1);
     const id = data[0].id;
     await supabase.from('uptime-check-record').delete().lte('id',500+id);
 
@@ -54,14 +56,31 @@ const checkRam = async (supabase,interval) => {
   }
  
   const yupAccountInfo = await eosApi.getAccount('yupyupyupyup');
+  const yupAccountInfoManager = await eosApi.getAccount('yupaccounts1');
+ 
+  const ramOverUsedForYupProtocol = (yupAccountInfo.ram_quota - yupAccountInfo.ram_usage) < mimimumRamRequired;
+  const ramOverUsedForYupManager = (yupAccountInfoManager.ram_quota - yupAccountInfoManager.ram_usage) < mimimumRamRequiredManager;
 
-  if ((yupAccountInfo.ram_quota - yupAccountInfo.ram_usage) < mimimumRamRequired) {
+
+  if ( ramOverUsedForYupProtocol || ramOverUsedForYupManager ) {
       
   await supabase
   .from('uptime-check-record')
   .insert([
     { status: false }
   ])
+
+  await supabase
+  .from('yup-status-settings')
+  .insert([
+    { value: Date.now(),
+      'value-json': {
+        ramOverUsedForYupProtocol: ramOverUsedForYupProtocol,
+        ramOverUsedForYupManager: ramOverUsedForYupManager,
+      }
+       }
+  ])
+
 
   } else {
     
@@ -72,13 +91,22 @@ const checkRam = async (supabase,interval) => {
     ])
     
   }
+  if (ramOverUsedForYupProtocol || ramOverUsedForYupManager) {
+    const  dataNoOfFailedChecks  = (await supabase
+      .from('yup-status-settings')
+      .select('*')
+      .eq('id', 3)).data[0];
+      let noChecks = Number(dataNoOfFailedChecks.value);
+      noChecks++
+      await supabase.from('yup-status-settings').update({ value: noChecks }).eq('id', 1);
+  } else {
   const  dataNoChecks  = (await supabase
     .from('yup-status-settings')
     .select('*')
     .eq('id', 1)).data[0];
   let noChecks = Number(dataNoChecks.value);
   noChecks++;
-  await supabase.from('yup-status-settings').update({ value: noChecks }).eq('id', 1);
+  }
 
 }
 
